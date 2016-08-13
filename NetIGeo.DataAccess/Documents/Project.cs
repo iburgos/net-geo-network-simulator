0ae50817d04b82c9b88eae332937dc183c526678
i-burgos <i.burgos@coolblue.nl>
2016-08-13 18:59:53 +0200

    Add DataAccess project

---
+++ NetIGeo.DataAccess/Documents/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetIGeo.DataAccess.Documents
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime EditDate { get; set; }
+    }
+}
