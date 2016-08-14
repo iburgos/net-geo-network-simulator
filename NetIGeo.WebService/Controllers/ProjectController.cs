2020010e9ad7df70c4c97317efb8c1cba855336a
i-burgos <i.burgos@coolblue.nl>
2016-08-14 11:14:38 +0200

    Add end point to retrieve all projects

--- NetIGeo.WebService/Controllers/ProjectController.cs
+++ NetIGeo.WebService/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 using AutoMapper;
 using NetIGeo.Domain.Models;
 using NetIGeo.Domain.Services;
@@ -10,33 +11,46 @@ namespace NetIGeo.WebService.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IProjectCreationService _projectCreationService;
+        private readonly IProjectRetrieverService _projectRetrieverService;
 
         public ProjectController(IProjectCreationService projectCreationService,
+            IProjectRetrieverService projectRetrieverService,
             IMapper mapper)
         {
             _projectCreationService = projectCreationService;
+            _projectRetrieverService = projectRetrieverService;
             _mapper = mapper;
         }
 
         public IHttpActionResult Get()
         {
-            return InternalServerError();
+            IHttpActionResult result = InternalServerError();
+
+            var serviceResult = _projectRetrieverService.Get();
+            if (serviceResult.Success)
+                result = Ok(_mapper.Map<IEnumerable<ProjectDto>>(serviceResult.Contents));
+
+            return result;
         }
 
         [HttpPost]
-        public IHttpActionResult New([FromBody] ProjectDto project)
+        public
+        IHttpActionResult New(
+            [FromBody] ProjectDto project)
         {
             IHttpActionResult result = InternalServerError();
 
-            if (project != null)
+            if (
+                project
+                != null)
             {
                 var projectModel = _mapper.Map<ProjectModel>(project);
                 var serviceResult = _projectCreationService.Create(projectModel);
                 if (serviceResult.Success)
-                {
                     result = Ok(_mapper.Map<ProjectDto>(serviceResult.Contents));
-                }
             }
+            else
+                result = BadRequest();
 
             return result;
         }
