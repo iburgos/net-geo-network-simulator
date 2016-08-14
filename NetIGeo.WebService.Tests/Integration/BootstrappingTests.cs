ce1199f1bed8b30737a7f58aff0d5c6014e6c824
i-burgos <i.burgos@coolblue.nl>
2016-08-14 02:44:26 +0200

    Add Web Service Tests project

---
+++ NetIGeo.WebService.Tests/Integration/BootstrappingTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetIGeo.WebService;
+using NetIGeo.WebService.Controllers;
+
+namespace NetIGeo.Domain.Tests.Integration
+{
+    [TestClass]
+    public class BootstrappingTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var container = new UnityContainer();
+            var settings = new Settings();
+            container.AddExtension(new Bootstrapper(settings));
+            container.Resolve<ProjectController>();
+        }
+    }
+}
