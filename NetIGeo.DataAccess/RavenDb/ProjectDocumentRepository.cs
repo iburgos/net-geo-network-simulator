1c89f0f54b6f7130bcb11a45e6f065413b398079
iburgos <ignacio.burgos.escribano@gmail.com>
2016-08-24 08:15:50 +0200

    Add Delete method to ProjectDocumentRepository

--- NetIGeo.DataAccess/RavenDb/ProjectDocumentRepository.cs
+++ NetIGeo.DataAccess/RavenDb/ProjectDocumentRepository.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-
 using AutoMapper;
-
 using NetIGeo.DataAccess.Common;
 using NetIGeo.DataAccess.Documents;
 
@@ -12,43 +10,52 @@ namespace NetIGeo.DataAccess.RavenDb
         Result<ProjectDocument> Create(ProjectDocument projectDocument);
         Result<IEnumerable<ProjectDocument>> GetAll();
         Result<ProjectDocument> Get(int id);
+        bool Delete(int id);
     }
 
     public class ProjectDocumentRepository : IProjectDocumentRepository
     {
+        private readonly IDocumentDeleter _documentDeleter;
         private readonly IDocumentRetriever _documentRetriever;
         private readonly IDocumentStorer _documentStorer;
         private readonly IMapper _mapper;
         private readonly IResultCreator _resultCreator;
 
         public ProjectDocumentRepository(IDocumentStorer documentStorer,
-                                         IDocumentRetriever documentRetriever,
-                                         IResultCreator resultCreator,
-                                         IMapper mapper)
+            IDocumentRetriever documentRetriever,
+            IDocumentDeleter documentDeleter,
+            IResultCreator resultCreator,
+            IMapper mapper)
         {
             _documentStorer = documentStorer;
             _documentRetriever = documentRetriever;
+            _documentDeleter = documentDeleter;
             _resultCreator = resultCreator;
             _mapper = mapper;
         }
 
         public Result<ProjectDocument> Create(ProjectDocument projectDocument)
         {
-            Result<IDocument> storerResult = _documentStorer.Store(projectDocument);
+            var storerResult = _documentStorer.Store(projectDocument);
             return _resultCreator.Create(_mapper.Map<ProjectDocument>(storerResult.Contents), storerResult.Success);
         }
 
         public Result<IEnumerable<ProjectDocument>> GetAll()
         {
-            Result<IEnumerable<ProjectDocument>> result = _documentRetriever.GetAll<ProjectDocument>();
+            var result = _documentRetriever.GetAll<ProjectDocument>();
             return _resultCreator.Create(result.Contents, result.Success);
         }
 
         public Result<ProjectDocument> Get(int id)
         {
-            Result<ProjectDocument> result = _documentRetriever.Get<ProjectDocument>(id,
+            var result = _documentRetriever.Get<ProjectDocument>(id,
                 RavenDbConstants.PROJECT_DOCUMENT_TYPE);
             return _resultCreator.Create(result.Contents, result.Success);
+        }
+
+        public bool Delete(int id)
+        {
+            return _documentDeleter.Delete<ProjectDocument>(id, RavenDbConstants.PROJECT_DOCUMENT_TYPE);
         }
     }
 }
